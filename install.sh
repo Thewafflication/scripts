7e298a2cd509a2e2951ef484d6ee3d193137c4c2
Jordan Waughtal <jmwaughtal1@hotmail.com>
2017-01-21 12:59:15 -0600

    Now updates system before trying to install.

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sudo apt update
+sudo apt upgrade -y
 sudo apt install apache2 openssl-blacklist ssl-cert php5 php5-gd php5-json php5-mcrypt php5-mysqlnd php5-recode php5-readline mcrypt php5-xmlrpc php-pear mariadb-server git 
 
 sudo mysql -u root -p < sql.sql
