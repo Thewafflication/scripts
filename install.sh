434d9819fcdc922d8e3b4ee677a3dd2b649c304a
Thewafflication <jmwaughtal1@hotmail.com>
2017-01-21 13:49:07 -0600

    Now correctly fixes owner permissions

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ sudo mkdir /var/www/html/uploads/customer_files/
 sudo find /var/www/ -type d -exec chmod 775 {} \;
 sudo find /var/www/ -type f -exec chmod 644 {} \;
 #read -n1 -r -p "Fixed Permissions" key
-#sudo chown -R www-data:www-data /var/www/
-read -n1 -r -p "Fixed Owner!" key
+sudo chown -R www-data:www-data /var/www/
+#read -n1 -r -p "Fixed Owner!" key
 sudo systemctl restart apache2.service
 sudo systemctl status apache2.service
 
