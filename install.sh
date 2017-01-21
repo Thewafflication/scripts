a732e231c5628a17e7807e94cbca13159cb1eee3
Thewafflication <jmwaughtal1@hotmail.com>
2017-01-21 13:47:32 -0600

    Fixed My sql step

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install apache2 openssl-blacklist ssl-cert php5 php5-gd php5-json php5-mcrypt php5-mysqlnd php5-recode php5-readline mcrypt php5-xmlrpc php-pear mariadb-server git 
 
-sudo mysql -u root -p < sql.sql
+sudo mysql -p < sql.sql
 
 sudo perl -pi.bak -e 's/;date.timezone =/date.timezone = \"America\/Chicago\"/g' /etc/php5/apache2/php.ini 
 
